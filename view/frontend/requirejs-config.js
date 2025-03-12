a58e4c690565a5f7fa893db8753ce04b4888ec90
Joseph Leedy <joseph.leedy@boldcommerce.com>
2025-03-12 10:54:57 -0600

    Fix incorrect extension name (CHK-8007)

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ let config = {
     config: {
         mixins: {
             'Magento_Checkout/js/view/payment/list': {
-                'Bold_AmastyCheckoutCompatibility/js/view/payment/list-mixin': true
+                'Bold_BoosterAmastyCheckoutCompatibility/js/view/payment/list-mixin': true
             }
         }
     }
